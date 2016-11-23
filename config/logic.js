8dfc59e238cb9a68786ccfaf079d029bb6fb83bf
sandraduarte <sandramduarte83@gmail.com>
2016-11-23 04:01:52 -0500

    initial logic/sql

---
+++ config/logic.js
@@ -0,0 +1,52 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+
+//MYSQL SETTINGS
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'geekseek_db'
+});
+//CONNECT TO MYSQL
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Geek Seek");
+    console.log("---------------------------");
+    //SHOW START MENU
+    // start();
+displayUsers();
+displayGeeks();
+displaySkills();
+
+});
+
+var Geek = require ('./../config/logic.js');
+
+function displayUsers() {
+	 connection.query('SELECT * FROM Users', function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " | " + res[i].userType + " | " + res[i].username + " | " + res[i].zipCode);
+        }
+        console.log("------------------------------");
+    });
+}
+function displayGeeks() {
+ connection.query('SELECT * FROM Geeks', function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " | " + res[i].username + " | " + res[i].zipCode);
+        }
+        console.log("------------------------------");
+    });
+}
+
+function displaySkills(){
+	 connection.query('SELECT * FROM Skills', function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].skillName);
+        }
+        console.log("------------------------------");
+    });
+}

5cb871ac801bc91e569494fcc61fbb6ce3bb5679
zodcikin <zeynep.odcikin@gmail.com>
2016-12-03 01:12:40 -0500

    heroku deployment

--- db/schema.sql
+++ db/schema.sql
@@ -1,59 +1,69 @@
-CREATE DATABASE GeekSeek_db;
-USE GeekSeek_db;
-
-
-CREATE TABLE Geek
+CREATE TABLE Users
 (
-	id int NOT NULL AUTO_INCREMENT,
-	username varchar(100) NOT NULL,
-	zipCode varchar(10),
-	HTML varchar(5),
-	CSS varchar(5),
-	JAVASCRIPT varchar(5),
-	MYSQL varchar(5),
-	NODE varchar(5),
-	created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (id)
+    id INTEGER  NOT NULL AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    zipCode varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    salt varchar(255) NOT NULL,
+    createdAt TIMESTAMP, 
+    updatedAt TIMESTAMP, 
+    deletedAt TIMESTAMP,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Seeker
+CREATE TABLE Geeks
 (
-	id int NOT NULL AUTO_INCREMENT,
-	username varchar(100) NOT NULL,
-	zipCode varchar(10),
-	companyName varchar(100),
-	devoured boolean not null default 0,
-	created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (id)
+    id INTEGER  NOT NULL AUTO_INCREMENT,
+    -- username varchar(100) NOT NULL,
+    -- zipCode varchar(10),
+    html INTEGER,
+    css INTEGER,
+    javascript INTEGER,
+    mysql INTEGER,
+    node INTEGER,
+    UserId INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (UserId) REFERENCES Users(id)
+    -- FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-CREATE TABLE Skill
+CREATE TABLE Seekers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	skillName varchar(100) NOT NULL,
-	difficulty integer default 0,
-	active boolean not null default 1,
-	created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (id)
+    id INTEGER  NOT NULL AUTO_INCREMENT,
+    -- username varchar(100) NOT NULL,
+    -- zipCode varchar(10),
+    companyName varchar(255),
+    updatedAt TIMESTAMP, 
+    deletedAt TIMESTAMP, 
+    UserId INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (UserId) REFERENCES Users(id)
 );
 
-CREATE TABLE GeekSkill
+CREATE TABLE Querys
 (
-	id int NOT NULL AUTO_INCREMENT,
-	GeekId int NOT NULL,
-	SkillId int NOT NULL,
-	Rating int,
-	created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (id)
+    id INTEGER  NOT NULL AUTO_INCREMENT,
+    queryName varchar(255) NOT NULL,
+    html INTEGER,
+    css INTEGER,
+    javascript INTEGER,
+    mysql INTEGER,
+    node INTEGER,
+    UserId INTEGER,
+    -- created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (UserId) REFERENCES Users(id)
 );
 
-CREATE TABLE SeekerSkill
+CREATE TABLE Skills
 (
-	id int NOT NULL AUTO_INCREMENT,
-	SeekerId int NOT NULL,
-	SkillId int NOT NULL,
-	Rating int,
-	Weight int,
-	created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (id)
+    id INTEGER  NOT NULL AUTO_INCREMENT,
+    skillName varchar(100) NOT NULL,
+    difficulty integer default 0,
+    active boolean not null default 1,
+    createdAt TIMESTAMP, 
+    updatedAt TIMESTAMP,
+    UserId INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (UserId) REFERENCES Users(id)
 );
--- models/geek.js
+++ models/geek.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Geek = sequelize.define('Geek', {
-    geekid: {
+    id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true

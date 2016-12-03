71b21cb601a79b8b14847a69d2d5e85d1ca91714
sandraduarte <sandramduarte83@gmail.com>
2016-12-03 05:16:48 -0500

    functionality check

--- controllers/geekseek_controller.js
+++ controllers/geekseek_controller.js
@@ -112,18 +112,15 @@ router.post('/newquery', function(req, res) {
 
 router.put('/query/insert/:id', function(req, res) {
     console.log('updating query for user: ' + req.params.id);
-    Query.create({queryName: req.body.queryName, 
+    Query.Create({queryName: req.body.queryName, 
                  html: req.body.q1,
                  css: req.body.q2, //should be q2's value
                  javascript: req.body.q3,
                  mysql: req.body.q4,
                  node: req.body.q5,
                  UserId: req.params.id
-                }
-              //   {where: 
-              //     {UserId: req.params.id}
-              // }
-              )
+                })
+   
     .then(function(){
         res.redirect('/savedqueries/'+req.params.id);
     });
